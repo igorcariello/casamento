a8f438a8869d5f65599bbd442877a4fd58533382
cariello <igor_cariello@hotmail.com.br>
2025-05-13 16:30:09 -0300

    reuptaded image OurHistory

--- src/components/Carousel/styles.ts
+++ src/components/Carousel/styles.ts
@@ -1,23 +1,21 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const Container = styled.div`
   display: none;
   width: 100%;
   margin-top: 3rem;
 
-
   @media (width <= 712px) {
-    display:flex;
+    display: flex;
     justify-content: center;
   }
-
-
 `
 
 export const SlideImage = styled.img`
-  width :100%;
-  object-fit: cover;
+  width: 100%;
+  height: auto;
   aspect-ratio: 9 / 16;
+  object-fit: cover;
   border-radius: 8px;
+  display: block;
 `
-
