4e864a5c308c9eaeac7636a2b3fe7204bf1b6d1d
cariello <igor_cariello@hotmail.com.br>
2025-07-06 18:10:21 -0300

    change axios address

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,9 +3,8 @@ import axios from 'axios'
 const token = localStorage.getItem("@casamentoBiaEIago:admin_token")
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3334',
+  baseURL: 'https://backend-casamento.onrender.com', // <-- Substitua pela URL REAL do seu backend no Render
   headers: {
     Authorization: `Bearer ${token}`
   }
 })
-
