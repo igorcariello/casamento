1b62c1d76c9d81df33d2621eed21cb568e59e997
cariello <igor_cariello@hotmail.com.br>
2025-07-07 23:52:49 -0300

    change axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,15 +2,8 @@ import axios from 'axios'
 
 const token = localStorage.getItem("@casamentoBiaEIago:admin_token")
 
-/*export const api = axios.create({
-  baseURL: 'https://backend-casamento.onrender.com',
-  headers: {
-    Authorization: `Bearer ${token}`
-  }
-})*/
-
 export const api = axios.create({
-  baseURL: 'http://localhost:3334',
+  baseURL: 'https://backend-casamento.onrender.com',
   headers: {
     Authorization: `Bearer ${token}`
   }
